d93895b3558c6b4e58f4507e09ab917f033f7459
Leonardo Timkang Jr. <locodafux@gmail.com>
2026-03-15 14:05:46 +0800

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Force HTTPS - Fix mixed content -->
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
